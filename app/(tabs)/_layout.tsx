10d662bced46e4e91580fa74b5487b04476043ea
wisdom Njimogu <njimoguwisdom@gmail.com>
2026-02-21 04:33:02 +0100

    created the tabs todo and settings

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,35 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+import { Ionicons } from "@expo/vector-icons";
+const TabsLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "purple",
+        tabBarInactiveTintColor: "gray",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Todos",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="flash-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,24 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.content}>welcome</Text>
+      <Text>this is the best solution app</Text>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "blue",
+  },
+  content: {
+    fontSize: 22,
+  },
+});
